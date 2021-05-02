cd9ab3b7fdb3add5d4e41c562b6ee00e027fcb83
NilsHMeier <66554930+NilsHMeier@users.noreply.github.com>
2021-05-02 22:03:52 +0200

    Adjust docstrings and code documentation

--- Python3Code/Chapter2/CreateDataset.py
+++ Python3Code/Chapter2/CreateDataset.py
@@ -18,8 +18,8 @@ import pathlib
 
 class CreateDataset:
     def __init__(self, base_dir: pathlib.Path, granularity: int):
-        self.BASE_DIR = base_dir
-        self.GRANULARITY = granularity
+        self.base_dir = base_dir
+        self.granularity = granularity
         self.data_table = None
 
     def create_timestamps(self, start_time: datetime, end_time: datetime) -> pd.DatetimeIndex:
@@ -31,17 +31,16 @@ class CreateDataset:
         :return: Pandas DatetimeIndex with the timestamps.
         """
 
-        return pd.date_range(start_time, end_time, freq=str(self.GRANULARITY) + 'ms')
+        return pd.date_range(start_time, end_time, freq=f'{self.granularity}ms')
 
-    def create_dataset(self, start_time: datetime, end_time: datetime, cols: list, prefix: str = '') -> None:
+    def create_dataset(self, start_time: datetime, end_time: datetime, cols: list, prefix: str = ''):
         """
-        Create empty pandas dataframe with cols as columns and timestamps as index.
+        Create empty pandas dataframe with cols as columns and timestamps as index and save it to instance attribute.
 
         :param start_time: Start time for the index in datetime format.
         :param end_time: End time for the index in datetime format.
         :param cols: Columns that will be created in dataframe.
         :param prefix: String that will be added to column names.
-        :return: Pandas dataframe with timestamps as index and columns.
         """
 
         if prefix != '':
@@ -49,41 +48,41 @@ class CreateDataset:
         timestamps = self.create_timestamps(start_time, end_time)
         self.data_table = pd.DataFrame(index=timestamps, columns=cols)
 
-    # Add numerical data, we assume timestamps in the form of nanoseconds from the epoch
     def add_numerical_dataset(self, file: str, timestamp_col: str, value_cols: list, aggregation: str = 'avg',
                               prefix: str = ''):
         """
-        Add numerical dataset to the aggregated dataset with instance granularity.
+        Add numerical dataset to the instance dataset with defined granularity. Timestamps are assumed to be in the
+        format of nanoseconds.
 
         :param file: Name of the file in base directory with the numerical data.
         :param timestamp_col: Name of the column containing timestamps in nanoseconds.
         :param value_cols: Columns of the dataset that should be used for aggregation.
         :param aggregation: Aggregation type. By now only 'avg' is supported.
-        :param prefix: String to add before column names in new aggregated dataset.
+        :param prefix: String to add before column names in aggregated dataset.
         """
 
         print(f'Reading data from {file}')
-        dataset = pd.read_csv(self.BASE_DIR / file, skipinitialspace=True)
+        dataset = pd.read_csv(self.base_dir / file, skipinitialspace=True)
 
         # Convert timestamps to dates
         dataset[timestamp_col] = pd.to_datetime(dataset[timestamp_col])
 
-        # Create a table based on the times found in the dataset
+        # Create a table based on the time found in the dataset
         if self.data_table is None:
             self.create_dataset(min(dataset[timestamp_col]), max(dataset[timestamp_col]), value_cols, prefix)
         else:
             for col in value_cols:
                 self.data_table[f'{prefix}{col}'] = np.nan
 
-        # Over all rows in the new table
+        # Iterate over all rows in the new table
         for timestamp in tqdm(self.data_table.index):
-            # Select the relevant measurements.
+            # Select the relevant measurements
             relevant_rows = dataset[
                 (dataset[timestamp_col] >= timestamp) &
-                (dataset[timestamp_col] < (timestamp + timedelta(milliseconds=self.GRANULARITY)))
-            ]
+                (dataset[timestamp_col] < (timestamp + timedelta(milliseconds=self.granularity)))
+                ]
             for col in value_cols:
-                # Take the average value
+                # Take the average value if rows are present
                 if len(relevant_rows) > 0:
                     if aggregation == 'avg':
                         self.data_table.loc[timestamp, f'{prefix}{col}'] = np.average(relevant_rows[col])
@@ -103,12 +102,10 @@ class CreateDataset:
 
         return re.sub('[^0-9a-zA-Z]+', '', name)
 
-    #
-    # 'aggregation' can be 'sum' or 'binary'.
     def add_event_dataset(self, file: str, start_timestamp_col: str, end_timestamp_col: str, value_col: str,
                           aggregation: str = 'sum'):
         """
-        Add data which have rows indicating the occurrence of a certain event with a given start and end time.
+        Add data which indicates the occurrence of a certain event between a given start and end time.
 
         :param file: Name of the file in base directory with the numerical data.
         :param start_timestamp_col: Name of the column containing the start timestamp for a event period.
@@ -118,9 +115,9 @@ class CreateDataset:
         """
 
         print(f'Reading data from {file}')
-        dataset = pd.read_csv(self.BASE_DIR / file)
+        dataset = pd.read_csv(self.base_dir / file)
 
-        # Convert timestamps to datetime.
+        # Convert timestamps to datetime
         dataset[start_timestamp_col] = pd.to_datetime(dataset[start_timestamp_col])
         dataset[end_timestamp_col] = pd.to_datetime(dataset[end_timestamp_col])
 
@@ -135,16 +132,16 @@ class CreateDataset:
         for col in event_values:
             self.data_table[f'{value_col}{col}'] = 0
 
-        # Now we need to start counting by passing along the rows....
+        # Start counting by passing along the rows
         for i in tqdm(range(0, len(dataset.index))):
-            # Identify the time points of the row in our dataset and the value
+            # Identify the time points of the row in the dataset and get the event value
             start = dataset[start_timestamp_col][i]
             end = dataset[end_timestamp_col][i]
             value = dataset[value_col][i]
 
             # Get the right rows from our data table
             relevant_rows = self.data_table[
-                (start <= (self.data_table.index + timedelta(milliseconds=self.GRANULARITY))) &
+                (start <= (self.data_table.index + timedelta(milliseconds=self.granularity))) &
                 (end > self.data_table.index)]
 
             # and add 1 to the rows if we take the sum
@@ -155,12 +152,3 @@ class CreateDataset:
                 self.data_table.loc[relevant_rows.index, f'{value_col}{value}'] = 1
             else:
                 raise ValueError(f'Unknown aggregation {aggregation}')
-
-    # This function returns the column names that have one of the strings expressed by 'ids' in the column name.
-    # TODO: Function is not used. May be deleted?!?
-    def get_relevant_columns(self, ids: list) -> list:
-        relevant_dataset_cols = []
-        cols = list(self.data_table.columns)
-        for i in ids:
-            relevant_dataset_cols.extend([col for col in cols if i in col])
-        return relevant_dataset_cols
--- Python3Code/crowdsignals_ch2.py
+++ Python3Code/crowdsignals_ch2.py
@@ -3,7 +3,7 @@
 #    Mark Hoogendoorn and Burkhardt Funk (2017)              #
 #    Machine Learning for the Quantified Self                #
 #    Springer                                                #
-#    Chapter 2                                               #
+#    Chapter 2: Basics of Sensory Data                       #
 #                                                            #
 ##############################################################
 
@@ -16,106 +16,116 @@ import copy
 import os
 import sys
 
-# Chapter 2: Initial exploration of the dataset.
 
-"""
-First, we set some module-level constants to store our data locations. These are saved as a pathlib.Path object, the
-preferred way to handle OS paths in Python 3 (https://docs.python.org/3/library/pathlib.html). Using the Path's methods,
-you can execute most path-related operations such as making directories.
+def main():
+    """
+    First, we set some constants to store our data locations. These are saved as a pathlib.Path object, the preferred
+    way to handle OS paths in Python 3 (https://docs.python.org/3/library/pathlib.html). Using the Path's methods,
+    the most path-related operations such as making directories can be executed.
 
-sys.argv contains a list of keywords entered in the command line, and can be used to specify a file path when running
-a script from the command line. For example:
+    sys.argv contains a list of keywords entered in the command line, and can be used to specify a file path when
+    running a script from the command line. For example:
 
-$ python3 crowdsignals_ch2.py my/proj/data/folder my_dataset.csv
+    "$ python3 crowdsignals_ch2.py my/proj/data/folder my_dataset.csv"
 
-If no location is specified, the default locations in the else statement are chosen, which are set to load each script's
-output into the next by default.
-"""
+    If no location is specified, the default locations in the else statement are chosen, which are set to load each
+    script's output into the next by default. If required all aggregated datasets can be saved by setting the
+    SAVE_VERSIONS attribute.
+    """
 
-DATASET_PATH = Path(sys.argv[1] if len(sys.argv) > 1 else './datasets/crowdsignals/csv-participant-one/')
-RESULT_PATH = Path('./intermediate_datafiles/')
-RESULT_FNAME = sys.argv[2] if len(sys.argv) > 2 else 'chapter2_result.csv'
+    DATASET_PATH = Path(sys.argv[1] if len(sys.argv) > 1 else './datasets/crowdsignals/csv-participant-one/')
+    RESULT_PATH = Path('./intermediate_datafiles/')
+    RESULT_FNAME = sys.argv[2] if len(sys.argv) > 2 else 'chapter2_result.csv'
 
-# Set a granularity (the discrete step size of our time series data). We'll use a course-grained granularity of one
-# instance per minute, and a fine-grained one with four instances per second.
-GRANULARITIES = [60000, 250]
+    # Set a granularity (the discrete step size of our time series data) and choose if all resulting datasets should be
+    # saved. We'll use a course-grained granularity of one instance per minute, and a fine-grained one with four
+    # instances per second.
+    GRANULARITIES = [60000, 250]
+    SAVE_VERSIONS = False
 
-# We can call Path.mkdir(exist_ok=True) to make any required directories if they don't already exist.
-[path.mkdir(exist_ok=True, parents=True) for path in [DATASET_PATH, RESULT_PATH]]
+    # We can call Path.mkdir(exist_ok=True) to make any required directories if they don't already exist.
+    [path.mkdir(exist_ok=True, parents=True) for path in [DATASET_PATH, RESULT_PATH]]
 
-datasets = []
-for milliseconds_per_instance in GRANULARITIES:
-    print(f'Creating numerical datasets from files in {DATASET_PATH} using granularity {milliseconds_per_instance}.')
+    # Create object to visualize the data and save figures
+    DataViz = VisualizeDataset(module_path=__file__)
 
-    # Create an initial dataset object with the base directory for our data and a granularity
-    data_engineer = CreateDataset(base_dir=DATASET_PATH, granularity=milliseconds_per_instance)
+    datasets = []
+    for milliseconds_per_instance in GRANULARITIES:
+        print(
+            f'Creating numerical datasets from files in {DATASET_PATH} using granularity {milliseconds_per_instance}.')
 
-    # Add the selected measurements to it.
+        # Create an initial dataset object with the base directory for our data and a granularity and add selected
+        # measurements to it
+        data_engineer = CreateDataset(base_dir=DATASET_PATH, granularity=milliseconds_per_instance)
 
-    # We add the accelerometer data (continuous numerical measurements) of the phone and the smartwatch
-    # and aggregate the values per timestep by averaging the values
-    data_engineer.add_numerical_dataset(file='accelerometer_phone.csv', timestamp_col='timestamps',
-                                        value_cols=['x', 'y', 'z'], aggregation='avg', prefix='acc_phone_')
-    data_engineer.add_numerical_dataset(file='accelerometer_smartwatch.csv', timestamp_col='timestamps',
-                                        value_cols=['x', 'y', 'z'], aggregation='avg', prefix='acc_watch_')
+        # Add the accelerometer data (continuous numerical measurements) of the phone and the smartwatch
+        # and aggregate the values per timestep by averaging the values
+        data_engineer.add_numerical_dataset(file='accelerometer_phone.csv', timestamp_col='timestamps',
+                                            value_cols=['x', 'y', 'z'], aggregation='avg', prefix='acc_phone_')
+        data_engineer.add_numerical_dataset(file='accelerometer_smartwatch.csv', timestamp_col='timestamps',
+                                            value_cols=['x', 'y', 'z'], aggregation='avg', prefix='acc_watch_')
 
-    # We add the gyroscope data (continuous numerical measurements) of the phone and the smartwatch
-    # and aggregate the values per timestep by averaging the values
-    data_engineer.add_numerical_dataset(file='gyroscope_phone.csv', timestamp_col='timestamps',
-                                        value_cols=['x', 'y', 'z'], aggregation='avg', prefix='gyr_phone_')
-    data_engineer.add_numerical_dataset(file='gyroscope_smartwatch.csv', timestamp_col='timestamps',
-                                        value_cols=['x', 'y', 'z'], aggregation='avg', prefix='gyr_watch_')
+        # Add the gyroscope data (continuous numerical measurements) of the phone and the smartwatch
+        # and aggregate the values per timestep by averaging the values
+        data_engineer.add_numerical_dataset(file='gyroscope_phone.csv', timestamp_col='timestamps',
+                                            value_cols=['x', 'y', 'z'], aggregation='avg', prefix='gyr_phone_')
+        data_engineer.add_numerical_dataset(file='gyroscope_smartwatch.csv', timestamp_col='timestamps',
+                                            value_cols=['x', 'y', 'z'], aggregation='avg', prefix='gyr_watch_')
 
-    # We add the heart rate (continuous numerical measurements) and aggregate by averaging again
-    data_engineer.add_numerical_dataset(file='heart_rate_smartwatch.csv', timestamp_col='timestamps',
-                                        value_cols=['rate'], aggregation='avg', prefix='hr_watch_')
+        # Add the heart rate (continuous numerical measurements) and aggregate by averaging the values
+        data_engineer.add_numerical_dataset(file='heart_rate_smartwatch.csv', timestamp_col='timestamps',
+                                            value_cols=['rate'], aggregation='avg', prefix='hr_watch_')
 
-    # We add the labels provided by the users. These are categorical events that might overlap. We add them
-    # as binary attributes (i.e. add a one to the attribute representing the specific value for the label if it
-    # occurs within an interval).
-    data_engineer.add_event_dataset(file='labels.csv', start_timestamp_col='label_start', end_timestamp_col='label_end',
-                                    value_col='label', aggregation='binary')
+        # Add the labels provided by the users as binary attributes (i.e. add a one to the attribute representing the
+        # specific value for a label if it occurs within an interval). These are categorical events that might overlap.
+        data_engineer.add_event_dataset(file='labels.csv', start_timestamp_col='label_start',
+                                        end_timestamp_col='label_end',
+                                        value_col='label', aggregation='binary')
 
-    # We add the amount of light sensed by the phone (continuous numerical measurements) and aggregate by averaging
-    data_engineer.add_numerical_dataset(file='light_phone.csv', timestamp_col='timestamps', value_cols=['lux'],
-                                        aggregation='avg', prefix='light_phone_')
+        # Add the amount of light sensed by the phone (continuous numerical measurements) and aggregate by averaging
+        data_engineer.add_numerical_dataset(file='light_phone.csv', timestamp_col='timestamps', value_cols=['lux'],
+                                            aggregation='avg', prefix='light_phone_')
 
-    # We add the magnetometer data (continuous numerical measurements) of the phone and the smartwatch
-    # and aggregate the values per timestep by averaging the values
-    data_engineer.add_numerical_dataset(file='magnetometer_phone.csv', timestamp_col='timestamps',
-                                        value_cols=['x', 'y', 'z'], aggregation='avg', prefix='mag_phone_')
-    data_engineer.add_numerical_dataset(file='magnetometer_smartwatch.csv', timestamp_col='timestamps',
-                                        value_cols=['x', 'y', 'z'], aggregation='avg', prefix='mag_watch_')
+        # Add the magnetometer data (continuous numerical measurements) of the phone and the smartwatch
+        # and aggregate the values per timestep by averaging the values
+        data_engineer.add_numerical_dataset(file='magnetometer_phone.csv', timestamp_col='timestamps',
+                                            value_cols=['x', 'y', 'z'], aggregation='avg', prefix='mag_phone_')
+        data_engineer.add_numerical_dataset(file='magnetometer_smartwatch.csv', timestamp_col='timestamps',
+                                            value_cols=['x', 'y', 'z'], aggregation='avg', prefix='mag_watch_')
 
-    # We add the pressure sensed by the phone (continuous numerical measurements) and aggregate by averaging again
-    data_engineer.add_numerical_dataset(file='pressure_phone.csv', timestamp_col='timestamps', value_cols=['pressure'],
-                                        aggregation='avg', prefix='press_phone_')
+        # Add the pressure sensed by the phone (continuous numerical measurements) and aggregate by averaging again
+        data_engineer.add_numerical_dataset(file='pressure_phone.csv', timestamp_col='timestamps',
+                                            value_cols=['pressure'],
+                                            aggregation='avg', prefix='press_phone_')
 
-    # Get the resulting pandas data table
-    dataset = data_engineer.data_table
+        # Get the resulting pandas data table
+        dataset = data_engineer.data_table
 
-    # Plot the data
-    DataViz = VisualizeDataset(__file__)
+        # Create boxplots
+        DataViz.plot_dataset_boxplot(dataset=dataset, cols=['acc_phone_x', 'acc_phone_y', 'acc_phone_z', 'acc_watch_x',
+                                                            'acc_watch_y', 'acc_watch_z'])
 
-    # Boxplot
-    DataViz.plot_dataset_boxplot(dataset=dataset, cols=['acc_phone_x', 'acc_phone_y', 'acc_phone_z', 'acc_watch_x',
-                                                        'acc_watch_y', 'acc_watch_z'])
+        # Plot all data
+        DataViz.plot_dataset(data_table=dataset,
+                             columns=['acc_', 'gyr_', 'hr_watch_rate', 'light_phone_lux', 'mag_', 'press_phone_',
+                                      'label'],
+                             match=['like', 'like', 'like', 'like', 'like', 'like', 'like', 'like'],
+                             display=['line', 'line', 'line', 'line', 'line', 'line', 'points', 'points'])
 
-    # Plot all data
-    DataViz.plot_dataset(data_table=dataset,
-                         columns=['acc_', 'gyr_', 'hr_watch_rate', 'light_phone_lux', 'mag_', 'press_phone_', 'label'],
-                         match=['like', 'like', 'like', 'like', 'like', 'like', 'like', 'like'],
-                         display=['line', 'line', 'line', 'line', 'line', 'line', 'points', 'points'])
+        # Print a summary of the dataset.
+        util.print_statistics(dataset=dataset)
+        datasets.append(copy.deepcopy(dataset))
 
-    # And print a summary of the dataset.
-    util.print_statistics(dataset=dataset)
-    datasets.append(copy.deepcopy(dataset))
+        # Save the various versions of the created datasets with logical filenames if needed
+        if SAVE_VERSIONS:
+            dataset.to_csv(RESULT_PATH / f'chapter2_result_{milliseconds_per_instance}')
 
-    # If needed, we could save the various versions of the dataset we create in the loop with logical filenames:
-    # dataset.to_csv(RESULT_PATH / f'chapter2_result_{milliseconds_per_instance}')
+    # Make a table like the one shown in the book, comparing the two datasets produced.
+    util.print_latex_table_statistics_two_datasets(dataset1=datasets[0], dataset2=datasets[1])
 
-# Make a table like the one shown in the book, comparing the two datasets produced.
-util.print_latex_table_statistics_two_datasets(dataset1=datasets[0], dataset2=datasets[1])
+    # Finally, store the last dataset we generated (250 ms).
+    dataset.to_csv(RESULT_PATH / RESULT_FNAME)
 
-# Finally, store the last dataset we generated (250 ms).
-dataset.to_csv(RESULT_PATH / RESULT_FNAME)
+
+if __name__ == '__main__':
+    main()
