73d03d448b57565b79ff4d0f63e4297294509277
NilsHMeier <66554930+NilsHMeier@users.noreply.github.com>
2021-06-25 18:23:37 +0200

    Adjust main scripts to upstream changes - Add argument parser to choose which part to run - Remove unnecessary imports - Fix minor bugs

--- Python3Code/crowdsignals_ch3_outliers.py
+++ Python3Code/crowdsignals_ch3_outliers.py
@@ -11,7 +11,6 @@ from util.VisualizeDataset import VisualizeDataset
 from Chapter3.OutlierDetection import DistributionBasedOutlierDetection
 from Chapter3.OutlierDetection import DistanceBasedOutlierDetection
 import sys
-import copy
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -21,6 +20,11 @@ import argparse
 DATA_PATH = Path('./intermediate_datafiles/')
 DATASET_FILENAME = sys.argv[1] if len(sys.argv) > 1 else 'chapter2_result.csv'
 RESULT_FILENAME = sys.argv[2] if len(sys.argv) > 2 else 'chapter3_result_outliers.csv'
+
+
+def print_flags():
+    for key, value in vars(FLAGS).items():
+        print(key + ' : ' + str(value))
 
 
 def main():
@@ -42,50 +46,81 @@ def main():
     # Set the columns to experiment on
     outlier_columns = ['acc_phone_x', 'light_phone_lux']
 
-    # Investigate the approaches for the relevant attributes
-    for col in outlier_columns:
-        print(f"Applying outlier criteria for column {col}")
+    if FLAGS.mode == 'chauvenet':
+        # Investigate the outlier columns using chauvenet criterium
+        for col in outlier_columns:
+            print(f"Applying chauvenet outlier criteria for column {col}")
+            dataset = OutlierDistribution.chauvenet(data_table=dataset, col=col)
+            DataViz.plot_binary_outliers(data_table=dataset, col=col, outlier_col=f'{col}_outlier')
 
-        # Try out different approaches. The parameters for each approach have been optimized by visual inspection
-        dataset = OutlierDistribution.chauvenet(data_table=dataset, col=col)
-        DataViz.plot_binary_outliers(data_table=dataset, col=col, outlier_col=f'{col}_outlier')
-        dataset = OutlierDistribution.mixture_model(data_table=dataset, col=col, components=3)
-        DataViz.plot_dataset(data_table=dataset, columns=[col, col + '_mixture'], match=['exact', 'exact'],
-                             display=['line', 'points'])
-
-        # This step requires:
-        # n_data_points * n_data_points * point_size = 31839 * 31839 * 32 bits = ~4GB available memory
-        try:
-            dataset = OutlierDistance.simple_distance_based(data_table=dataset, cols=[col], d_function='euclidean',
-                                                            d_min=0.10, f_min=0.99)
-            DataViz.plot_binary_outliers(data_table=dataset, col=col, outlier_col='simple_dist_outlier')
-        except MemoryError:
-            print('Not enough memory available for simple distance-based outlier detection...')
-            print('Skipping.')
-
-        try:
-            dataset = OutlierDistance.local_outlier_factor(data_table=dataset, cols=[col], d_function='euclidean', k=5)
-            DataViz.plot_dataset(data_table=dataset, columns=[col, 'lof'], match=['exact', 'exact'],
+    elif FLAGS.mode == 'mixture':
+        # Investigate the outlier columns using mixture models
+        for col in outlier_columns:
+            print(f"Applying mixture model for column {col}")
+            dataset = OutlierDistribution.mixture_model(data_table=dataset, col=col, components=3)
+            DataViz.plot_dataset(data_table=dataset, columns=[col, f'{col}_mixture'], match=['exact', 'exact'],
                                  display=['line', 'points'])
-        except MemoryError:
-            print('Not enough memory available for local outlier factor...')
-            print('Skipping.')
 
-        # Remove all the stuff from the dataset again
-        cols_to_remove = [col + '_outlier', col + '_mixture', 'simple_dist_outlier', 'lof']
-        for to_remove in cols_to_remove:
-            if to_remove in dataset:
-                del dataset[to_remove]
+    elif FLAGS.mode == 'distance':
+        for col in outlier_columns:
+            print(f"Applying distance based outlier detection for column {col}")
+            # This step requires:
+            # n_data_points * n_data_points * point_size = 31839 * 31839 * 32 bits = ~4GB available memory
+            try:
+                dataset = OutlierDistance.simple_distance_based(data_table=dataset, cols=[col], d_function='euclidean',
+                                                                d_min=FLAGS.dmin, f_min=FLAGS.fmin)
+                DataViz.plot_binary_outliers(data_table=dataset, col=col, outlier_col='simple_dist_outlier')
+            except MemoryError:
+                print('Not enough memory available for simple distance-based outlier detection...')
+                print('Skipping.')
 
-    # Step 2: Take Chauvenet's criterion and apply it to all but the label column in the main dataset
-    for col in [c for c in dataset.columns if 'label' not in c]:
-        print(f'Measurement is now: {col}')
-        dataset = OutlierDistribution.chauvenet(data_table=dataset, col=col)
-        dataset.loc[dataset[f'{col}_outlier'], col] = np.nan
-        del dataset[col + '_outlier']
+    elif FLAGS.mode == 'LOF':
+        for col in outlier_columns:
+            print(f"Applying Local outlier factor for column {col}")
+            try:
+                dataset = OutlierDistance.local_outlier_factor(data_table=dataset, cols=[col], d_function='euclidean',
+                                                               k=FLAGS.K)
+                DataViz.plot_dataset(data_table=dataset, columns=[col, 'lof'], match=['exact', 'exact'],
+                                     display=['line', 'points'])
+            except MemoryError:
+                print('Not enough memory available for local outlier factor...')
+                print('Skipping.')
 
-    dataset.to_csv(DATA_PATH / RESULT_FILENAME)
+    elif FLAGS.mode == 'final':
+        # Take Chauvenet's criterion and apply it to all but the label column in the main dataset
+        for col in [c for c in dataset.columns if 'label' not in c]:
+            print(f'Measurement is now: {col}')
+            dataset = OutlierDistribution.chauvenet(data_table=dataset, col=col)
+            dataset.loc[dataset[f'{col}_outlier'], col] = np.nan
+            del dataset[col + '_outlier']
+
+        dataset.to_csv(DATA_PATH / RESULT_FILENAME)
 
 
 if __name__ == '__main__':
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--mode', type=str, default='final',
+                        help='Select what version to run: LOF, distance, mixture, chauvenet or final \
+                            "LOF" applies the Local Outlier Factor to a single variable \
+                            "distance" applies a distance based outlier detection method to a single variable \
+                            "mixture" applies a mixture model to detect outliers for a single variable\
+                            "chauvenet" applies Chauvenet outlier detection method to a single variable \
+                            "final" is used for the next chapter',
+                        choices=['LOF', 'distance', 'mixture', 'chauvenet', 'final'])
+
+    parser.add_argument('--K', type=int, default=5,
+                        help='Local Outlier Factor:  K is the number of neighboring points considered')
+
+    parser.add_argument('--dmin', type=float, default=0.10,
+                        help='Simple distance based:  dmin is ... ')
+
+    parser.add_argument('--fmin', type=float, default=0.99,
+                        help='Simple distance based:  fmin is ... ')
+
+    FLAGS, unparsed = parser.parse_known_args()
+
+    # Print args and run main script
+    print_flags()
     main()
--- Python3Code/crowdsignals_ch4.py
+++ Python3Code/crowdsignals_ch4.py
@@ -9,6 +9,7 @@
 
 import sys
 import copy
+import argparse
 import pandas as pd
 from pathlib import Path
 from util.VisualizeDataset import VisualizeDataset
@@ -20,6 +21,11 @@ from Chapter4.FrequencyAbstraction import FourierTransformation
 DATA_PATH = Path('./intermediate_datafiles/')
 DATASET_FNAME = sys.argv[1] if len(sys.argv) > 1 else 'chapter3_result_final.csv'
 RESULT_FNAME = sys.argv[2] if len(sys.argv) > 2 else 'chapter4_result.csv'
+
+
+def print_flags():
+    for key, value in vars(FLAGS).items():
+        print(key + ' : ' + str(value))
 
 
 def main():
@@ -37,79 +43,106 @@ def main():
     # Compute the number of milliseconds covered by an instance based on the first two rows
     milliseconds_per_instance = (dataset.index[1] - dataset.index[0]).microseconds / 1000
 
-    # Chapter 4: Identifying aggregate attributes
-
-    # Focus on the time domain first
-
-    # Set the window sizes to the number of instances representing 5 seconds, 30 seconds and 5 minutes
-    window_sizes = [int(float(5000) / milliseconds_per_instance), int(float(0.5 * 60000) / milliseconds_per_instance),
-                    int(float(5 * 60000) / milliseconds_per_instance)]
-
+    # Create objects for feature abstraction
     NumAbs = NumericalAbstraction()
-    dataset_copy = copy.deepcopy(dataset)
-    for ws in window_sizes:
-        print(f'Abstracting numerical features for window size {ws*milliseconds_per_instance/1000}s.')
-        dataset_copy = NumAbs.abstract_numerical(data_table=dataset_copy, cols=['acc_phone_x'], window_size=ws,
-                                                 aggregation_function='mean')
-        dataset_copy = NumAbs.abstract_numerical(data_table=dataset_copy, cols=['acc_phone_x'], window_size=ws,
-                                                 aggregation_function='std')
-
-    DataViz.plot_dataset(data_table=dataset_copy,
-                         columns=['acc_phone_x', 'acc_phone_x_temp_mean', 'acc_phone_x_temp_std', 'label'],
-                         match=['exact', 'like', 'like', 'like'], display=['line', 'line', 'line', 'points'])
-
-    ws = int(float(0.5 * 60000) / milliseconds_per_instance)
-    selected_predictor_cols = [c for c in dataset.columns if 'label' not in c]
-    print('Calculating mean and std for selected predictor cols.')
-    dataset = NumAbs.abstract_numerical(data_table=dataset, cols=selected_predictor_cols, window_size=ws,
-                                        aggregation_function='mean')
-    dataset = NumAbs.abstract_numerical(data_table=dataset, cols=selected_predictor_cols, window_size=ws,
-                                        aggregation_function='std')
-
-    DataViz.plot_dataset(data_table=dataset, columns=['acc_phone_x', 'gyr_phone_x', 'hr_watch_rate', 'light_phone_lux',
-                                                      'mag_phone_x', 'press_phone_', 'pca_1', 'label'],
-                         match=['like', 'like', 'like', 'like', 'like', 'like', 'like', 'like'],
-                         display=['line', 'line', 'line', 'line', 'line', 'line', 'line', 'points'])
-
     CatAbs = CategoricalAbstraction()
-    print('Abstracting categorical features.')
-    dataset = CatAbs.abstract_categorical(data_table=dataset, cols=['label'], match=['like'], min_support=0.03,
-                                          window_size=int(float(5 * 60000) / milliseconds_per_instance),
-                                          max_pattern_size=2)
-
-    # Move to the frequency domain with the same window size
     FreqAbs = FourierTransformation()
-    fs = 1000.0 / milliseconds_per_instance
 
-    periodic_predictor_cols = ['acc_phone_x', 'acc_phone_y', 'acc_phone_z', 'acc_watch_x', 'acc_watch_y', 'acc_watch_z',
-                               'gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z', 'gyr_watch_x', 'gyr_watch_y', 'gyr_watch_z',
-                               'mag_phone_x', 'mag_phone_y', 'mag_phone_z', 'mag_watch_x', 'mag_watch_y', 'mag_watch_z']
-    data_table = FreqAbs.abstract_frequency(data_table=copy.deepcopy(dataset), cols=['acc_phone_x'],
-                                            window_size=int(float(10000) / milliseconds_per_instance), sampling_rate=fs)
+    if FLAGS.mode == 'time':
+        # Focus on the time domain first
+        # Set the window sizes to the number of instances representing 5 seconds, 30 seconds and 5 minutes
+        window_sizes = [int(float(5000) / milliseconds_per_instance),
+                        int(float(0.5 * 60000) / milliseconds_per_instance),
+                        int(float(5 * 60000) / milliseconds_per_instance)]
 
-    # Spectral analysis
-    DataViz.plot_dataset(data_table=data_table,
-                         columns=['acc_phone_x_max_freq', 'acc_phone_x_freq_weighted', 'acc_phone_x_pse', 'label'],
-                         match=['like', 'like', 'like', 'like'], display=['line', 'line', 'line', 'points'])
+        dataset_copy = copy.deepcopy(dataset)
+        for ws in window_sizes:
+            print(f'Abstracting numerical features for window size {ws * milliseconds_per_instance / 1000}s.')
+            dataset_copy = NumAbs.abstract_numerical(data_table=dataset_copy, cols=['acc_phone_x'], window_size=ws,
+                                                     aggregation_function='mean')
+            dataset_copy = NumAbs.abstract_numerical(data_table=dataset_copy, cols=['acc_phone_x'], window_size=ws,
+                                                     aggregation_function='std')
 
-    print('Abstracting frequency features.')
-    dataset = FreqAbs.abstract_frequency(data_table=dataset, cols=periodic_predictor_cols,
-                                         window_size=int(float(10000) / milliseconds_per_instance), sampling_rate=fs)
+        DataViz.plot_dataset(data_table=dataset_copy,
+                             columns=['acc_phone_x', 'acc_phone_x_temp_mean', 'acc_phone_x_temp_std', 'label'],
+                             match=['exact', 'like', 'like', 'like'], display=['line', 'line', 'line', 'points'])
 
-    # Take a certain percentage of overlap in the windows, otherwise training examples will be too much alike.
-    # Set the allowed percentage of overlap
-    window_overlap = 0.9
-    skip_points = int((1 - window_overlap) * ws)
-    dataset = dataset.iloc[::skip_points, :]
+    elif FLAGS.mode == 'frequency':
+        # Move to the frequency domain with the same window size
+        fs = 1000.0 / milliseconds_per_instance
+        ws = int(10000.0 / milliseconds_per_instance)
 
-    DataViz.plot_dataset(data_table=dataset, columns=['acc_phone_x', 'gyr_phone_x', 'hr_watch_rate', 'light_phone_lux',
-                                                      'mag_phone_x', 'press_phone_', 'pca_1', 'label'],
-                         match=['like', 'like', 'like', 'like', 'like', 'like', 'like', 'like'],
-                         display=['line', 'line', 'line', 'line', 'line', 'line', 'line', 'points'])
+        data_table = FreqAbs.abstract_frequency(data_table=copy.deepcopy(dataset), cols=['acc_phone_x'],
+                                                window_size=ws, sampling_rate=fs)
+        # Spectral analysis
+        DataViz.plot_dataset(data_table=data_table,
+                             columns=['acc_phone_x_max_freq', 'acc_phone_x_freq_weighted', 'acc_phone_x_pse', 'label'],
+                             match=['like', 'like', 'like', 'like'], display=['line', 'line', 'line', 'points'])
 
-    # Store the generated dataset
-    dataset.to_csv(DATA_PATH / RESULT_FNAME)
+    elif FLAGS.mode == 'final':
+        ws = int(float(0.5 * 60000) / milliseconds_per_instance)
+        fs = 1000.0 / milliseconds_per_instance
+
+        # Abstract time domain features and plot the result
+        selected_predictor_cols = [c for c in dataset.columns if 'label' not in c]
+        print('Calculating mean and std for selected predictor cols.')
+        dataset = NumAbs.abstract_numerical(data_table=dataset, cols=selected_predictor_cols, window_size=ws,
+                                            aggregation_function='mean')
+        dataset = NumAbs.abstract_numerical(data_table=dataset, cols=selected_predictor_cols, window_size=ws,
+                                            aggregation_function='std')
+
+        DataViz.plot_dataset(data_table=dataset, columns=['acc_phone_x', 'gyr_phone_x', 'hr_watch_rate',
+                                                          'light_phone_lux', 'mag_phone_x', 'press_phone_',
+                                                          'pca_1', 'label'],
+                             match=['like', 'like', 'like', 'like', 'like', 'like', 'like', 'like'],
+                             display=['line', 'line', 'line', 'line', 'line', 'line', 'line', 'points'])
+
+        # Abstract categorical features
+        print('Abstracting categorical features.')
+        dataset = CatAbs.abstract_categorical(data_table=dataset, cols=['label'], match=['like'], min_support=0.03,
+                                              window_size=int(float(5 * 60000) / milliseconds_per_instance),
+                                              max_pattern_size=2)
+
+        # Abstract frequency domain features
+        periodic_predictor_cols = ['acc_phone_x', 'acc_phone_y', 'acc_phone_z', 'acc_watch_x', 'acc_watch_y',
+                                   'acc_watch_z', 'gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z', 'gyr_watch_x',
+                                   'gyr_watch_y', 'gyr_watch_z', 'mag_phone_x', 'mag_phone_y', 'mag_phone_z',
+                                   'mag_watch_x', 'mag_watch_y', 'mag_watch_z']
+
+        print('Abstracting frequency features.')
+        dataset = FreqAbs.abstract_frequency(data_table=dataset, cols=periodic_predictor_cols,
+                                             window_size=ws, sampling_rate=fs)
+
+        # Take a certain percentage of overlap in the windows, otherwise training examples will be too much alike
+        # Set the allowed percentage of overlap
+        window_overlap = FLAGS.overlap
+        skip_points = int((1 - window_overlap) * ws)
+        dataset = dataset.iloc[::skip_points, :]
+
+        # Plot the final dataset
+        DataViz.plot_dataset(data_table=dataset,
+                             columns=['acc_phone_x', 'gyr_phone_x', 'hr_watch_rate', 'light_phone_lux',
+                                      'mag_phone_x', 'press_phone_', 'pca_1', 'label'],
+                             match=['like', 'like', 'like', 'like', 'like', 'like', 'like', 'like'],
+                             display=['line', 'line', 'line', 'line', 'line', 'line', 'line', 'points'])
+
+        # Store the generated dataset
+        dataset.to_csv(DATA_PATH / RESULT_FNAME)
 
 
 if __name__ == '__main__':
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, default='final',
+                        help='Select what version to run: final, aggregation or freq \
+                            "time" studies the effect of several aggeregation methods in the time domain \
+                            "frequency" applies a Fast Fourier transformation to a single variable \
+                            "final" is used for the next chapter ',
+                        choices=['time', 'frequency', 'final'])
+    parser.add_argument('--overlap', type=float, default=0.9,
+                        help='Allowed overlap percentage for windows in dataset')
+    FLAGS, unparsed = parser.parse_known_args()
+
+    # Print args and run main script
+    print_flags()
     main()
