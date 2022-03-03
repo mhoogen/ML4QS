dfc21bce90312943afe0d716edaacdd7be0cde2b
Anne Fischer <37384664+AnneFischer@users.noreply.github.com>
2022-03-03 10:13:24 +0100

    Update VisualizeDataset.py

--- Python3Code/util/VisualizeDataset.py
+++ Python3Code/util/VisualizeDataset.py
@@ -35,7 +35,7 @@ class VisualizeDataset:
         self.figures_dir.mkdir(exist_ok=True, parents=True)
 
 
-    def save(self, plot_obj, formats=('png', 'eps', 'pdf')): # 'svg'
+    def save(self, plot_obj, formats=('png', 'pdf')): # 'svg'
 
         fig_name = f'figure_{self.plot_number}'
 
